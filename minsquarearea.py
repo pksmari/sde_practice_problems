24d8aca2264785b45fe9012b4dee0b96cd0b8099
HariHaran-24 <58470609+HariHaran-24@users.noreply.github.com>
2020-06-08 09:41:49 +0530

    Delete minsquarearea.py

--- minsquarearea.py
+++
@@ -1,9 +0,0 @@
-for t in range(int(input())):
-  n=int(input())
-  x=[]
-  y=[]
-  for i in range(n):
-    u,v=map(int,input().split())
-    x.append(u)
-    y.append(v)
-  print(max(abs(max(x)-min(x)),abs(max(y)-min(y)))**2)
